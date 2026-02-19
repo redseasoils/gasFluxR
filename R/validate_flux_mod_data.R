6ce7612622894fc950e40067ba3ce73a79151902
redseasoils <moses8@illinois.edu>
2026-02-19 10:58:28 -0700

    revised order of data validation
    checking seconds[1] = 0 before checking NA values led to failures when all seconds values are NA, so I put the NA checks first

--- R/validate_flux_mod_data.R
+++ R/validate_flux_mod_data.R
@@ -5,15 +5,6 @@ validate_flux_mod_data <- function(flux_mod, allow_all_zero = FALSE) {
   seconds <- flux_mod$seconds_raw
   min_n <- flux_mod$min_n
 
-  # Check that seconds start at 0
-  if (seconds[1] != 0) {
-    return(list(
-      valid = FALSE,
-      possible = FALSE,
-      reason = "`seconds` must start at 0"
-    ))
-  }
-
   # Check minimum non-NA values
   non_na <- sum(!is.na(ppm) & !is.na(seconds))
   if (non_na < 2) {
@@ -23,6 +14,15 @@ validate_flux_mod_data <- function(flux_mod, allow_all_zero = FALSE) {
     return(list(
       valid = FALSE, possible = TRUE,
       reason = paste0("Non-NA values (", non_na, ") < min_n (", min_n, ")")
+    ))
+  }
+
+  # Check that seconds start at 0
+  if (seconds[1] != 0) {
+    return(list(
+      valid = FALSE,
+      possible = FALSE,
+      reason = "`seconds` must start at 0"
     ))
   }
 
